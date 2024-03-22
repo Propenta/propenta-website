41be2ac520638bb0617dbb3d32524dec123acec5
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-03-22 08:22:14 +0100

    Essaie du fonctionnement du formulaire de contact

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,27 @@
+document.getElementById("contactForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let subject = document.getElementById("subject").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    fetch("send_email.php", {
+        method: "POST",
+        headers : {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body : "subjet=" + encodeURIComponent(nom) + "&email=" + encodeURIComponent(email) + "&message=" + encodeURIComponent(message)
+    })
+    .then(response => response.text())
+    .then(data => {
+        alert(data);
+        document.getElementById("contactFrom").reset();
+    })
+    .catch(error => console.error("Erreur : ",error));
+    
+});
+
+
 function kill_space(){
     if(window.innerWidth <= 768)
     {
@@ -13,4 +37,4 @@ function kill_space(){
 
 kill_space()
 
-window.addEventListener('resize', kill_space)
+window.addEventListener('resize', kill_space);
